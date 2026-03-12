3d85e5981d544965972d488565680e9d6e446063
admin <admin@cloudconcepts4u.in>
2026-03-12 15:54:50 +0530

    fixing

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+def greet():
+    return "Hello from Docker + Kubernetes + ArgoCD!"
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello from Docker + Kubernetes + ArgoCD!")
+        self.wfile.write(greet().encode())
 
 def run():
     server = HTTPServer(("0.0.0.0", 8000), Handler)
